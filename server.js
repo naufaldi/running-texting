dedfa8f6b04e8757e4568058ef8bd76cbbed04c6
bill tanthowi jauhari <bill.tanthowi.j@gmail.com>
2016-05-25 00:27:31 +0700

    change index.html to rest.html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "rest.html"));
 });
 
 app.get('/bundle.js', (req, res) => {
